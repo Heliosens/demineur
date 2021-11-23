74c68909b1e2d662f7fa8a43cf11460ea60a97e4
Sylvie-B <unsolo@hotmail.fr>
2021-11-23 22:40:22 +0100

    create gameboard & bomb

--- appDem.js
+++ appDem.js
@@ -2,9 +2,10 @@
 let gameBoard = document.getElementById("gameBoard");
 let cases = gameBoard.getElementsByTagName("div");
 
-// choice column & line nbr
+// choice column & line nbr & number of bomb
 let col = 25;
-let line = 25;
+let line = 20;
+let nbrBomb = 10;
 
 // create col * lin div
 for(let i = 0 ; i < (col * line) ; i++){
@@ -20,3 +21,25 @@ for( let i = 0 ; i < cases.length ; i++){
     cases[i].style.width = 100 / col + "%";
 }
 
+// place X bomb
+let count = 0;
+for(let i = 0 ; i < nbrBomb ; i++){
+    let bomb = document.createElement('div');
+    bomb.className = "bomb";
+    bomb.style.backgroundColor = 'black';
+    bomb.style.border = '1px red solid';
+    bomb.style.width = '1.5rem';
+    bomb.style.height = '1.5rem';
+    let x = Math.round(Math.random() * cases.length);
+    console.log(x);
+    if(cases[x].children.length === 0){
+        cases[x].appendChild(bomb);
+        count++;
+        console.log(count);
+    }
+}
+
+// eval cases value in function of bomb
+for(let i = 0 ; i < cases.length ; i++){
+
+}
