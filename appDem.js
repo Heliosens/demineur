7fed7b8fa7fbb48b0851b481428122f84beff72b
Sylvie-B <unsolo@hotmail.fr>
2021-11-24 10:40:53 +0100

    place bombs, display total nbr

--- appDem.js
+++ appDem.js
@@ -2,10 +2,16 @@
 let gameBoard = document.getElementById("gameBoard");
 let cases = gameBoard.getElementsByTagName("div");
 
+// get text
+let nbrT = document.getElementById("nbrT");
+
+
 // choice column & line nbr & number of bomb
 let col = 25;
 let line = 20;
-let nbrBomb = 10;
+let nbrBomb = 50;
+nbrT.innerHTML = nbrBomb.toString();
+
 
 // create col * lin div
 for(let i = 0 ; i < (col * line) ; i++){
@@ -21,25 +27,27 @@ for( let i = 0 ; i < cases.length ; i++){
     cases[i].style.width = 100 / col + "%";
 }
 
-// place X bomb
-let count = 0;
 for(let i = 0 ; i < nbrBomb ; i++){
+
     let bomb = document.createElement('div');
     bomb.className = "bomb";
     bomb.style.backgroundColor = 'black';
     bomb.style.border = '1px red solid';
     bomb.style.width = '1.5rem';
     bomb.style.height = '1.5rem';
+    placeBomb(bomb);
+}
+
+// place X bomb
+function placeBomb (element){
     let x = Math.round(Math.random() * cases.length);
     console.log(x);
     if(cases[x].children.length === 0){
-        cases[x].appendChild(bomb);
-        count++;
-        console.log(count);
+        cases[x].appendChild(element);
+    }
+    else {
+        placeBomb(element);
     }
 }
 
 // eval cases value in function of bomb
-for(let i = 0 ; i < cases.length ; i++){
-
-}
